9fbed2f22db4314022a82c6d3c3e02539d0928c1
el_brujas <el_brujas@Neo-ukM>
2020-07-20 18:05:59 -0300

    Mire, hice otro cambio

--- src/main/java/edu/fiuba/algo3/modelo/Pantalla.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pantalla.java
@@ -3,5 +3,5 @@ package edu.fiuba.algo3.modelo;
 public class Pantalla {
 	//Esta es una clse pantalla, haria cositas.. si supiera usar java
 	//segundo commit por si no funciono el anterior
-
+	//Este es otro cambio
 }
