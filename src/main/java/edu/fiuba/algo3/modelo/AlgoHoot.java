9b3bba3be1ec2590ce6495f3b987e285c962fc17
Abraham <aosco@fi.uba.ar>
2020-07-20 13:08:23 -0300

    inicio del tp2

---
+++ src/main/java/edu/fiuba/algo3/modelo/AlgoHoot.java
@@ -0,0 +1,22 @@
+
+package edu.fiuba.algo3.modelo;
+
+import java.util.ArrayList;
+
+public class AlgoHoot {
+    private ArrayList<Jugador> jugadores;
+    private ArrayList<Pregunta> preguntas;
+
+    public AlgoHoot(){
+        this.jugadores=new ArrayList<Jugador>();
+        this.preguntas=new ArrayList<Pregunta>();
+
+    }
+    public void inciarJuego(){
+
+    }
+    public void cargarPreguntas(ArrayList<Pregunta> preguntas){
+        this.preguntas=preguntas;
+    }
+
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/Jugador.java
@@ -0,0 +1,14 @@
+package edu.fiuba.algo3.modelo;
+
+public class Jugador {
+    private int puntaje;
+    private String nombre;
+
+    public Jugador(){
+        this.puntaje=0;
+        this.nombre='jugador1';
+
+    }
+
+}
+
