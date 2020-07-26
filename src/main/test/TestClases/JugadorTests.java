3ad6ad67e99613f6a34f82817ab7073290c2bd7d
matja <mausqui@fi.uba.ar>
2020-07-26 02:04:37 -0300

    Añadidos test de las herederas de opciones, Jugador y Ronda

---
+++ src/main/test/TestClases/JugadorTests.java
@@ -0,0 +1,69 @@
+package TestClases;
+
+import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JugadorTests {
+
+    @Test
+    public void test01getNombre() {
+
+        Jugador unJugador = new Jugador ("Akos");
+        assertEquals("Akos",unJugador.getNombre());
+
+    }
+    @Test
+    public void Test02setNombre() {
+
+        Jugador unJugador = new Jugador ("ciertoNombre");
+        unJugador.setNombre("Wardruna");
+        assertEquals(unJugador.getNombre(),"Wardruna");
+
+    }
+    @Test
+    public void Test03generarRespuesta() {
+
+        Jugador unJugador = new Jugador ("Electaurus");
+
+        Opcion unaOpcion = new OpcionCorrecta("Hemos de arremeter al alba.");
+        Opcion otraOpcion = new OpcionIncorrecta("Cargaremos a la media mañana");
+
+        ArrayList<Opcion> opciones = new ArrayList();
+
+        opciones.add(unaOpcion);
+        opciones.add(otraOpcion);
+
+        Respuesta respuestasUnJugador = unJugador.generarRespuesta(opciones);
+
+        assertEquals(respuestasUnJugador.getRemitente().getNombre(),unJugador.getNombre());
+        for (int i = 0; i < opciones.size(); i++) {
+
+            assertEquals(respuestasUnJugador.getOpcionesElegidas().get(i).getTexto(),opciones.get(i).getTexto());
+
+        }
+
+    }
+    @Test
+    public void Test04getPuntaje() {
+
+        Jugador unJugador = new Jugador("IlCapitaniJackSpaguetti");
+        assertEquals(0,unJugador.getPuntaje());
+
+    }
+    @Test
+    public void Test05acumularPuntaje() {
+
+        Jugador unJugador = new Jugador("Heimerdingar");
+
+        int nuevosPuntos = 2;
+        unJugador.acumularPuntaje(nuevosPuntos);
+
+        assertEquals(2, unJugador.getPuntaje());
+
+    }
+
+}
---
+++ src/main/test/TestClases/OpcionIncorrectaTests.java
@@ -0,0 +1,27 @@
+package TestClases;
+
+import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OpcionIncorrectaTests {
+
+    @Test
+    public void test01getTexto() {
+
+        Opcion unaOpcion = new OpcionIncorrecta("Cuatro cardos valen doce doblones");
+
+        assertEquals("Cuatro cardos valen doce doblones",unaOpcion.getTexto());
+
+    }
+    @Test
+    public void test02devolverPuntaje() {
+
+        Opcion unaOpcion = new OpcionIncorrecta("Un sabio oriental camina apresuradamente por la orilla del Tiber");
+
+        assertEquals(0, unaOpcion.devolverPuntaje());
+
+    }
+
+}
