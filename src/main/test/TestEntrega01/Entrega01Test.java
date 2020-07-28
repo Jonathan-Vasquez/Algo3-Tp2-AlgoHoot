d183b5063e855ffecd880d4300670638ae7cf66c
Jonathan-Vasquez <jhuata@fi.uba.ar>
2020-07-28 08:53:02 -0300

    Se Organizo el codigo de los test de Entrega01Test  en Arrange Act Assert

--- src/main/test/TestEntrega01/Entrega01Test.java
+++ src/main/test/TestEntrega01/Entrega01Test.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class Entrega01Test {
     @Test
     public void test01PreguntaDeVerdaderoFalsoPenalizablePuedeCrearseIndicandoleCualEsLaRespuestaCorrecta(){
+        //Arrange
         Opcion opcionCorrecta = new Opcion("si");
         Opcion opcionIncorrecta = new Opcion("no");
 
@@ -26,10 +27,14 @@ public class Entrega01Test {
         TipoPregunta tipoPreguntaVerdaderoFalsoPenalizable = new PreguntaVerdaderoFalsoPenalizable();
         Pregunta unaPregunta = new Pregunta("se debe hacer test?", tipoPreguntaVerdaderoFalsoPenalizable, listaDeOpciones);
 
-        assertTrue(unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta));
+        //Act
+        boolean resultado = unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta);
+        //Assert
+        assertTrue(resultado);
     }
     @Test
     public void test02PreguntaDeVerdaderoFalsoPenalizableRecibeLasOpcionesElegidasPorLosJugadoresEntoncesAsignaBienSusPuntos(){
+        //Arrange
         Opcion opcionCorrecta = new Opcion("si");
         Opcion opcionIncorrecta = new Opcion("no");
 
@@ -41,32 +46,36 @@ public class Entrega01Test {
         opcionesCorrectas.add(opcionCorrecta);
 
         ListaOpcionesParaPregunta listaDeOpciones = new ListaOpcionesParaPregunta(opcionesAMostrar, opcionesCorrectas);
-
         TipoPregunta tipoPreguntaVerdaderoFalsoPenalizable = new PreguntaVerdaderoFalsoPenalizable();
         Pregunta unaPregunta = new Pregunta("se debe testear?", tipoPreguntaVerdaderoFalsoPenalizable, listaDeOpciones);
 
         //jugadores eligen opciones
         ArrayList<Opcion> opcionesElegidasJugador1 = new ArrayList<>();
         opcionesElegidasJugador1.add(opcionCorrecta);
-
         ArrayList<Opcion> opcionesElegidasJugador2 = new ArrayList<>();
         opcionesElegidasJugador2.add(opcionIncorrecta);
 
-        assertEquals(1, unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador1));
-        assertEquals(-1, unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador2));
+        //Act
+        int puntajeParaJugador1 = unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador1);
+        int puntajeParaJugador2 = unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador2);
+        //Assert
+        assertEquals(1, puntajeParaJugador1);
+        assertEquals(-1, puntajeParaJugador2);
     }
+
     @Test
     public void test03PreguntaMultipleChoiceClasicoPuedeCrearseIndicandoleCualEsLaRespuestaCorrecta(){
-        Opcion opcionCorrecta1 = new Opcion("chile");
-        Opcion opcionCorrecta2 = new Opcion("ecuador");
+        //Arrange
+        Opcion opcionCorrecta1 = new Opcion("Chile");
+        Opcion opcionCorrecta2 = new Opcion("Ecuador");
+        Opcion opcionCorrecta3 = new Opcion("Argentina");
         Opcion opcionIncorrecta = new Opcion("Estados Unidos");
-        Opcion opcionCorrecta3 = new Opcion("argentina");
 
         ArrayList<Opcion> opcionesAMostrar = new ArrayList<>();
         opcionesAMostrar.add(opcionCorrecta1);
         opcionesAMostrar.add(opcionCorrecta2);
-        opcionesAMostrar.add(opcionIncorrecta);
         opcionesAMostrar.add(opcionCorrecta3);
+        opcionesAMostrar.add(opcionIncorrecta);
 
         ArrayList<Opcion> opcionesCorrectas = new ArrayList<>();
         opcionesCorrectas.add(opcionCorrecta1);
@@ -78,13 +87,20 @@ public class Entrega01Test {
         TipoPregunta tipoPreguntaMultipleChoice = new PreguntaMultipleChoice();
         Pregunta unaPregunta = new Pregunta("Cuales son paises de Sud America?",tipoPreguntaMultipleChoice, listaDeOpciones );
 
-        assertTrue(unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta1));
-        assertTrue(unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta2));
-        assertTrue(unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta3));
+        //Act
+        boolean contieneOpcionCorrecta1 = unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta1);
+        boolean contieneOpcionCorrecta2 = unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta2);
+        boolean contieneOpcionCorrecta3 = unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta3);
+
+        //Assert
+        assertTrue(contieneOpcionCorrecta1);
+        assertTrue(contieneOpcionCorrecta2);
+        assertTrue(contieneOpcionCorrecta3);
     }
 
     @Test
     public void test04PreguntaMultipleChoiceClasicoRecibeLasOpcionesElegidasPorLosJugadoresEntoncesAsignaBienSusPuntos(){
+        //Arrange
         Opcion opcionCorrecta1 = new Opcion("chile");
         Opcion opcionCorrecta2 = new Opcion("ecuador");
         Opcion opcionIncorrecta = new Opcion("Estados Unidos");
@@ -114,15 +130,20 @@ public class Entrega01Test {
 
         ArrayList<Opcion> opcionesElegidasJugador2 = new ArrayList<>();
         opcionesElegidasJugador2.add(opcionCorrecta1);
-        opcionesElegidasJugador2.add(opcionIncorrecta);
         opcionesElegidasJugador2.add(opcionCorrecta3);
+        opcionesElegidasJugador2.add(opcionIncorrecta);
 
-        assertEquals(1, unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador1));
-        assertEquals(0, unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador2));
+        //Act
+        int puntajeParaJugador1 = unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador1);
+        int puntajeParaJugador2 = unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador2);
+        //Assert
+        assertEquals(1, puntajeParaJugador1);
+        assertEquals(0, puntajeParaJugador2);
     }
 
     @Test
     public void test05PreguntaMultipleChoiceDePuntajeParcialPuedeCrearseIndicandoleCualEsLaRespuestaCorrecta(){
+        //Arrange
         Opcion opcionCorrecta1 = new Opcion("chile");
         Opcion opcionCorrecta2 = new Opcion("ecuador");
         Opcion opcionIncorrecta = new Opcion("Estados Unidos");
@@ -144,16 +165,23 @@ public class Entrega01Test {
         TipoPregunta tipoPreguntaMultipleChoicePuntajeParcial = new PreguntaMultipleChoicePuntajeParcial();
         Pregunta unaPregunta = new Pregunta("Cuales son paises de Sud America?", tipoPreguntaMultipleChoicePuntajeParcial, listaDeOpciones);
 
-        assertTrue(unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta1));
-        assertTrue(unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta2));
-        assertTrue(unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta3));
+        //Act
+        boolean contieneOpcionCorrecta1 = unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta1);
+        boolean contieneOpcionCorrecta2 = unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta2);
+        boolean contieneOpcionCorrecta3 = unaPregunta.getOpcionesCorrectas().contains(opcionCorrecta3);
+
+        //Assert
+        assertTrue(contieneOpcionCorrecta1);
+        assertTrue(contieneOpcionCorrecta2);
+        assertTrue(contieneOpcionCorrecta3);
     }
     @Test
     public void test06PreguntaMultipleChoiceDePuntajeParcialRecibeLasOpcionesElegidasPorLosJugadoresEntoncesAsignaBienSusPuntos(){
+        //Arrange
         Opcion opcionCorrecta1 = new Opcion("chile");
         Opcion opcionCorrecta2 = new Opcion("ecuador");
-        Opcion opcionIncorrecta = new Opcion("Estados Unidos");
         Opcion opcionCorrecta3 = new Opcion("argentina");
+        Opcion opcionIncorrecta = new Opcion("Estados Unidos");
 
         ArrayList<Opcion> opcionesAMostrar = new ArrayList<>();
         opcionesAMostrar.add(opcionCorrecta1);
@@ -181,7 +209,12 @@ public class Entrega01Test {
         opcionesElegidasJugador2.add(opcionIncorrecta);
         opcionesElegidasJugador2.add(opcionCorrecta3);
 
-        assertEquals(2, unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador1));
-        assertEquals(0, unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador2));
+
+        //Act
+        int puntajeParaJugador1 = unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador1);
+        int puntajeParaJugador2 = unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesElegidasJugador2);
+        //Assert
+        assertEquals(2, puntajeParaJugador1);
+        assertEquals(0, puntajeParaJugador2);
     }
 }
