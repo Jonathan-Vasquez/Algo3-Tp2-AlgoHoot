c88c4ec0d194f4ae9f81ed845128a501efac50e4
Jonathan-Vasquez <jhuata@fi.uba.ar>
2020-07-28 07:26:14 -0300

    Se agrego a PreguntaTest el test : test02ObtenerPuntajeOpcionesELejidasDeUnaListaVaciaDevuelveCeroConCualquierTipoPregunta

--- src/main/test/TestClases/PreguntaTest.java
+++ src/main/test/TestClases/PreguntaTest.java
@@ -1,12 +1,11 @@
 package TestClases;
 
 import edu.fiuba.algo3.modelo.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PreguntaTest {
 
@@ -33,10 +32,38 @@ public class PreguntaTest {
         ListaOpcionesParaPregunta listaOpcionesParaPregunta = new ListaOpcionesParaPregunta(opcionesApresentar,opcionesCorrectas);
 
         Pregunta unaPregunta = new Pregunta("¿Es bueno Hacer Tests?",new PreguntaMultipleChoice(), listaOpcionesParaPregunta);
-
         assertEquals("¿Es bueno Hacer Tests?", unaPregunta.getConsigna());
+    }
+    @Test
+    public void test02ObtenerPuntajeOpcionesELejidasDeUnaListaVaciaDevuelveCeroConCualquierTipoPregunta(){
+        //Average
+        Opcion opcionCorrectaUno = new Opcion("Seeee");
+        Opcion opcionIncorrecta = new Opcion("Nop");
+
+        ArrayList<Opcion> opcionesApresentar = new ArrayList<>();
+        opcionesApresentar.add(opcionCorrectaUno);
+        opcionesApresentar.add(opcionIncorrecta);
+
+        ArrayList<Opcion> opcionesCorrectas = new ArrayList<>();
+        opcionesCorrectas.add(opcionCorrectaUno);
+
+        ArrayList<Opcion> listaVacia = new ArrayList<>();
+
+        ListaOpcionesParaPregunta listaOpcionesParaPregunta = new ListaOpcionesParaPregunta(opcionesApresentar,opcionesCorrectas);
+
+        //Act
+        Pregunta preguntaVerdaderoFalsoClasico = new Pregunta("¿Es bueno Hacer Tests?",new PreguntaVerdaderoFalso(), listaOpcionesParaPregunta);
+        Pregunta preguntaVerdaderoFalsoPenalizble = new Pregunta("¿Es bueno Hacer Tests?",new PreguntaVerdaderoFalsoPenalizable(), listaOpcionesParaPregunta);
+        Pregunta preguntaMultipleChoiceClasico = new Pregunta("¿Es bueno Hacer Tests?",new PreguntaMultipleChoice(), listaOpcionesParaPregunta);
+        Pregunta preguntaMultipleChoicePuntajeParcial = new Pregunta("¿Es bueno Hacer Tests?",new PreguntaMultipleChoicePuntajeParcial(), listaOpcionesParaPregunta);
+
+
+        //Assert
+        assertEquals(0,preguntaVerdaderoFalsoClasico.obtenerPuntajeOpcionesElejidas(listaVacia));
+        assertEquals(0,preguntaVerdaderoFalsoPenalizble.obtenerPuntajeOpcionesElejidas(listaVacia));
+        assertEquals(0,preguntaMultipleChoiceClasico.obtenerPuntajeOpcionesElejidas(listaVacia));
+        assertEquals(0,preguntaMultipleChoicePuntajeParcial.obtenerPuntajeOpcionesElejidas(listaVacia));
+
 
     }
-
-
 }
