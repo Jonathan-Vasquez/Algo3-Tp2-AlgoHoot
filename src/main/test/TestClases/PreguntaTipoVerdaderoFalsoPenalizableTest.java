2df31f4106c907c9022cdb1a77bcec9ca78f42ea
Jonathan-Vasquez <jhuata@fi.uba.ar>
2020-07-28 07:39:01 -0300

    Se cambio el nombre de la clase Test PreguntaVerdaderoFalsoTest a PreguntaTipoVerdaderoFalsoTest

--- src/main/test/TestClases/PreguntaTipoVerdaderoFalsoPenalizableTest.java
+++ src/main/test/TestClases/PreguntaTipoVerdaderoFalsoPenalizableTest.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PreguntaVerdaderoFalsoPenalizableTest {
+public class PreguntaTipoVerdaderoFalsoPenalizableTest {
     @Test
     public void test01SeLeEnviaUnaOpcionCorrectaYDevuelveUnPunto(){
 
