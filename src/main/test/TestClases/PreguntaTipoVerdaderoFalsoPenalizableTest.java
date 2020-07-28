d25f9e2b7192876672866ea28baa084cce2c5d56
Jonathan-Vasquez <jhuata@fi.uba.ar>
2020-07-28 07:43:49 -0300

    Se actualizaron los test de PreguntaTipoVerdaderoFalsoPenalizableTest de acuerdo a su nuevo comportamiento

--- src/main/test/TestClases/PreguntaTipoVerdaderoFalsoPenalizableTest.java
+++ src/main/test/TestClases/PreguntaTipoVerdaderoFalsoPenalizableTest.java
@@ -9,66 +9,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PreguntaTipoVerdaderoFalsoPenalizableTest {
     @Test
     public void test01SeLeEnviaUnaOpcionCorrectaYDevuelveUnPunto(){
-
-        Opcion opcionIncorrecta = new Opcion("Nop");
+        //Arrange
         Opcion opcionCorrecta = new Opcion("Seeee");
-
-        ArrayList<Opcion> opcionesApresentar = new ArrayList<>();
-        opcionesApresentar.add(opcionCorrecta);
-        opcionesApresentar.add(opcionIncorrecta);
 
         ArrayList <Opcion> opcionesCorrectas = new ArrayList<>();
         opcionesCorrectas.add(opcionCorrecta);
-
-        ListaOpcionesParaPregunta listaOpcionesParaPregunta = new ListaOpcionesParaPregunta(opcionesApresentar,opcionesCorrectas);
-        Pregunta unaPregunta = new Pregunta("¿Es bueno Hacer Tests?",new PreguntaVerdaderoFalso(), listaOpcionesParaPregunta);
 
         ArrayList<Opcion> opcionesDeUnJugador = new ArrayList<>();
         opcionesDeUnJugador.add(opcionCorrecta);
 
-        assertEquals(1,unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesDeUnJugador));
+        TipoPregunta verdaderoFalsoPenalizable = new PreguntaVerdaderoFalsoPenalizable();
+        //Act
+        int resultado = verdaderoFalsoPenalizable.calcularPuntaje(opcionesDeUnJugador,opcionesCorrectas);
+
+        //Assert
+        assertEquals(1,resultado);
     }
 
     @Test
     public void test02SeLeEnviaUnaOpcionIncorrectaYDevuelveMenosUnPuntos(){
+        //Arrange
         Opcion opcionIncorrecta = new Opcion("Nop");
         Opcion opcionCorrecta = new Opcion("Seeee");
 
-        ArrayList<Opcion> opcionesApresentar = new ArrayList<>();
-        opcionesApresentar.add(opcionCorrecta);
-        opcionesApresentar.add(opcionIncorrecta);
-
         ArrayList <Opcion> opcionesCorrectas = new ArrayList<>();
         opcionesCorrectas.add(opcionCorrecta);
-
-        ListaOpcionesParaPregunta listaOpcionesParaPregunta = new ListaOpcionesParaPregunta(opcionesApresentar,opcionesCorrectas);
-        Pregunta unaPregunta = new Pregunta("¿Es bueno Hacer Tests?",new PreguntaVerdaderoFalsoPenalizable(), listaOpcionesParaPregunta);
 
         ArrayList<Opcion> opcionesDeUnJugador = new ArrayList<>();
         opcionesDeUnJugador.add(opcionIncorrecta);
 
-        assertEquals(-1,unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesDeUnJugador));
+        TipoPregunta verdaderoFalsoPenalizable = new PreguntaVerdaderoFalsoPenalizable();
+        //Act
+        int resultado = verdaderoFalsoPenalizable.calcularPuntaje(opcionesDeUnJugador,opcionesCorrectas);
+
+        //Assert
+        assertEquals(-1,resultado);
     }
-    @Test
-    public void test03SeLeEnviaOpcionesVacia () {
 
-        Opcion opcionIncorrecta = new Opcion("Nop");
-        Opcion opcionCorrecta = new Opcion("Seeee");
-
-        ArrayList<Opcion> opcionesApresentar = new ArrayList<>();
-        opcionesApresentar.add(opcionCorrecta);
-        opcionesApresentar.add(opcionIncorrecta);
-
-        ArrayList <Opcion> opcionesCorrectas = new ArrayList<>();
-        opcionesCorrectas.add(opcionCorrecta);
-
-        ListaOpcionesParaPregunta listaOpcionesParaPregunta = new ListaOpcionesParaPregunta(opcionesApresentar,opcionesCorrectas);
-        Pregunta unaPregunta = new Pregunta("¿Es bueno Hacer Tests?",new PreguntaVerdaderoFalso(), listaOpcionesParaPregunta);
-
-        ArrayList<Opcion> opcionesDeUnJugador = new ArrayList<>();
-
-        assertEquals(0,unaPregunta.obtenerPuntajeOpcionesElejidas(opcionesDeUnJugador));
-
-    }
 
 }
